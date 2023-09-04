20a96b7e73a83b3c7cc0e878caabc0841ca553f9
Margaret Paiva <margaretpaiva20@gmail.com>
2023-09-04 18:53:36 +0000

    Start the structure in correct.py

---
+++ mini-rnn/correct.py
@@ -0,0 +1,38 @@
+#This file is part of mini-rnn
+#Released under the MIT License (see LICENSE file)
+
+"""
+Run the assembling process
+"""
+
+import subprocess
+import logging
+
+logger = logging.getLogger()
+
+
+class AssembleException(Exception):
+    pass
+
+def assemble(input_filename, tmp_dir, threads):
+    
+    # the output of the assembly is placed in tmp_dir
+    cmd_line = [
+        'flye', '--nano-raw', input_filename,
+        '--out-dir', tmp_dir, '--threads', str(threads),
+    ]
+    try:
+        logger.debug("Running: " + " ".join(cmdline))
+        subprocess.check_call(cmdline)
+    except subprocess.CalledProcessError as e:
+        if e.returncode == -9:
+            logger.error("Looks like the system ran out of memory")
+        raise AssembleException(str(e))
+    except OSError as e:
+        raise AssembleException
+    
+    assemble_log = tmp_dir / 'flye.log'
+    with open(assemble_log, 'w') as stderr:
+        subprocess.call(command, stderr=stderr)
+    # can add the time taken for this process in the log
+    log('Assembly completed')
